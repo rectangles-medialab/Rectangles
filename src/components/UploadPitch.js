1d80492fc7fc2c54c7aec30b64a32ad92af64ab2
Rav1sh <ravishsomai808@gmail.com>
2023-03-21 15:58:48 +0100

    Content added
    Added Content for Landing page

--- src/components/UploadPitch.js
+++ src/components/UploadPitch.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import '../App.css';
+import StartProcessor from "./StartProcessor";
+import downloadImage from './download.png';
 
 export default function UploadPitch() {
     const [videoUrl, setVideoUrl] = useState('');
@@ -16,8 +18,8 @@ export default function UploadPitch() {
 
     return (
         <div className="app-container">
-            <h1>Laat het checken door PitchBack!</h1>
-            <p>PitchBack is een tool die jou helpt feedback te geven over  de pitch die je houdt. Hierbij geeft de tool feedbaak op postuur en spraak.</p>
+            <h1>Weet je niet zeker of jouw pitch goed overkomt bij het publiek?</h1>
+            <p>Laat het checken door PitchBack! Dit is een tool die jou helpt feedback te geven over de pitch die je houdt. Hierbij geeft de tool feedback op jouw postuur en spraak.</p>
 
             {videoUrl ? (
 
@@ -26,22 +28,25 @@ export default function UploadPitch() {
                         <video src={videoUrl} controls width="500" height="300" />
                     </div>
                     <div className="next-button-container">
-                        <p>Check jou video nog een keer voordat we jou video laat checken door PitchBack. Misschien zijn er nog een paar dingen die je wilt toevoegen of verwijderen?</p>
-                        <button className="next-button" onClick={handleNextSection}>Volgende</button>
+                        <h1>Video is geupload!</h1>
+                        <p>Bekijk jouw video nog een keer voordat we de video laten controleren door PitchBack.</p>
+                        <p>Misschien zijn er nog een aantal dingen die je wilt toevoegen of verwijderen?</p>
+                        <button className="next-button" onClick={handleNextSection}>PitchBack Check</button>
                     </div>
                 </div>
             ) : (
                 <div className="upload-container">
-                    <p>Upload jouw video</p>
-                    <input type="file" onChange={handleUpload} />
+                        <p>Upload hieronder jouw video</p>
+                        <input type="file" onChange={handleUpload} id="actual-btn" hiddenid="actual-btn" hidden/>
+                        <label for="actual-btn">
+                            <img src={downloadImage} alt="Download Button" />
+                        </label>
+                        {/* <span id="file-chosen">No file chosen</span> */}
                 </div>
             )}
 
-            {showNextSection && (
-                <div className="next-container">
-                    <p>Nice</p>
-                </div>
-            )}
+            {showNextSection && <StartProcessor />}
+
         </div>
     );
 }
