397b9a52722f7beabd13460e7b78434d468340a0
LBeukennoot <lars@beukenkamp.net>
2023-03-22 16:08:02 +0100

    Changed StartProcessor to FeedbackList

---
+++ src/components/FeedbackList.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function FeedbackList() {
+    return (
+        <div className="next-container">
+            <h1>Je Pitch is gecheckt!</h1>
+            <p>Bekijk hieronder de feedback op je postuur en spraak! Merkt u enkele fouten of problemen in de feedback? Laat het ons weten via npo@info.nl</p>
+
+            {/* TODO feedback cards maken en erin data van props doen */}
+        </div>
+    );
+}
+
+export default FeedbackList;
