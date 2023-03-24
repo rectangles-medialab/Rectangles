7d0aa7c1b9024adcce5d210396de0608cd38c698
NagalieArt <70587195+NagalieArt@users.noreply.github.com>
2023-03-24 12:33:32 +0100

    change in react audio

--- src/audio/audioInput.js
+++ src/audio/audioInput.js
@@ -1,60 +1,62 @@
-import React, { useState } from "react";
-import ffmpeg from "ffmpeg.js";
-import DeepSpeech from "deepspeech";
+import React, { useEffect, useState } from "react";
+import * as tf from "@tensorflow/tfjs";
+import * as speechCommands from "@tensorflow-models/speech-commands";
+import fs from "fs";
 
-const VoiceCounter = () => {
+function TeachableMachine() {
+  const [recognizer, setRecognizer] = useState(null);
+  const [classLabels, setClassLabels] = useState([]);
+  const [labelContainer, setLabelContainer] = useState(null);
 
-  const [selectedFile, setSelectedFile] = useState(null);
-  const [transcription, setTranscription] = useState("");
+  useEffect(() => {
+    async function loadModel() {
+      const modelURL = "https://teachablemachine.withgoogle.com/models/ptOfwlb7h/model.json";
+      const metadataURL = "https://teachablemachine.withgoogle.com/models/ptOfwlb7h/metadata.json";
+      const model = await tf.loadLayersModel(tf.io.browserFiles([modelURL, metadataURL]));
+      const commands = JSON.parse(await fs.promises.readFile(metadataURL));
 
-  const handleFileSelect = (event) => {
-    setSelectedFile(event.target.files[0]);
-  };
+      const recognizer = speechCommands.create("BROWSER_FFT", null, model);
+      await recognizer.ensureModelLoaded();
+      setRecognizer(recognizer);
+      setClassLabels(commands.wordLabels);
 
-  const transcribeAudio = async () => {
-    if (!selectedFile) {
-      return;
-    }
+      const container = document.getElementById("label-container");
+      setLabelContainer(container);
+      for (let i = 0; i < commands.wordLabels.length; i++) {
+        container.appendChild(document.createElement("div"));
+      }
 
-    try {
-      // Step 2: Read the contents of the file as an ArrayBuffer
-      const arrayBuffer = await new Promise((resolve, reject) => {
-        const reader = new FileReader();
-        reader.onload = (event) => {
-          resolve(event.target.result);
-        };
-        reader.onerror = reject;
-        reader.readAsArrayBuffer(selectedFile);
+      recognizer.listen((result) => {
+        const scores = result.scores;
+        for (let i = 0; i < classLabels.length; i++) {
+          const classPrediction = classLabels[i] + ": " + result.scores[i].toFixed(2);
+          labelContainer.childNodes[i].innerHTML = classPrediction;
+        }
+      }, {
+        includeSpectrogram: true,
+        probabilityThreshold: 0.75,
+        invokeCallbackOnNoiseAndUnknown: true,
+        overlapFactor: 0.50
       });
-
-      // Step 3: Convert the mp4 file to an audio file format using ffmpeg.js
-      const ffmpegInstance = await ffmpeg();
-      await ffmpegInstance.FS("writeFile", "input.mp4", new Uint8Array(arrayBuffer));
-      await ffmpegInstance.run("-i", "input.mp4", "output.wav");
-      const outputArrayBuffer = await ffmpegInstance.FS("readFile", "output.wav");
-
-      // Step 4: Transcribe the audio to text using Mozilla DeepSpeech
-      const model = new DeepSpeech.Model("path/to/deepspeech/model");
-      const scorer = new DeepSpeech.Scorer("path/to/deepspeech/scorer");
-      const audioBuffer = new Int16Array(outputArrayBuffer);
-      const result = model.stt(audioBuffer, scorer);
-      setTranscription(result);
-
-      // Step 5: Display the transcribed text to the user
-      // You could render it as plain text or use a text-to-speech library to read it aloud
-    } catch (error) {
-      console.error(error);
     }
-  };
+
+    loadModel();
+
+    return () => {
+      if (recognizer) {
+        recognizer.stopListening();
+      }
+    };
+  }, [recognizer, classLabels, labelContainer]);
 
   return (
     <div>
-      <input type="file" onChange={handleFileSelect} />
-      <button onClick={transcribeAudio}>Transcribe</button>
-      <div>{transcription}</div>
+      <div>Teachable Machine Audio Model</div>
+      <button type="button" onClick={() => recognizer && recognizer.listen()}>Start</button>
+      <div id="label-container"></div>
     </div>
   );
 }
 
-export default VoiceCounter;
+export default TeachableMachine;
 
