ba82259129c9da827e5af70a1647c00761762873
Rav1sh <ravishsomai808@gmail.com>
2023-03-24 11:58:47 +0100

    Added styling for the video section

--- src/components/UploadPitch.js
+++ src/components/UploadPitch.js
@@ -17,36 +17,43 @@ export default function UploadPitch() {
     }
 
     return (
-        <div className="app-container">
-            <h1>Weet je niet zeker of jouw pitch goed overkomt bij het publiek?</h1>
-            <p>Laat het checken door PitchBack! Dit is een tool die jou helpt feedback te geven over de pitch die je houdt. Hierbij geeft de tool feedback op jouw postuur en spraak.</p>
-
-            {videoUrl ? (
-
-                <div className="video-container">
-                    <div>
-                        <video src={videoUrl} controls width="500" height="300" />
-                    </div>
-                    <div className="next-button-container">
-                        <h1>Video is geupload!</h1>
-                        <p>Bekijk jouw video nog een keer voordat we de video laten controleren door PitchBack.</p>
-                        <p>Misschien zijn er nog een aantal dingen die je wilt toevoegen of verwijderen?</p>
-                        <button className="next-button" onClick={handleNextSection}>PitchBack Check</button>
-                    </div>
+        <div className="container">
+            <div className="app-container">
+                <div class="header">
+                <h1>Weet je niet zeker of jouw pitch goed overkomt bij het publiek?</h1>
+                <p>Laat het checken door PitchBack! Dit is een tool die jou helpt feedback te geven over de pitch die je houdt. Hierbij geeft de tool feedback op jouw postuur en spraak.</p>
                 </div>
-            ) : (
-                <div className="upload-container">
-                        <p>Upload hieronder jouw video</p>
-                        <input type="file" onChange={handleUpload} id="actual-btn" hiddenid="actual-btn" hidden/>
+                <div className="upload-video-container">
+                    <div className="upload-container">
+                        <p>Upload hieronder jouw video.</p>
+                        <input type="file" onChange={handleUpload} id="actual-btn" hiddenid="actual-btn" hidden />
                         <label for="actual-btn">
                             <img src={downloadImage} alt="Download Button" />
                         </label>
-                        {/* <span id="file-chosen">No file chosen</span> */}
+                    </div>
+
+                    {videoUrl && (
+                        <div className="main-container">
+                            <div className="video-container">
+                                <div>
+                                    <video src={videoUrl} controls width="500" height="300" />
+                                </div>
+                                <div className="next-button-container">
+                                    <h1>Video is geupload!</h1>
+                                    <p>Bekijk jouw video nog een keer voordat we de video laten controleren door PitchBack.</p>
+                                    <p>Misschien zijn er nog een aantal dingen die je wilt toevoegen of verwijderen?</p>
+                                </div>
+                            </div>
+                            <div className="new-section">
+                                <button className="next-button" onClick={handleNextSection}>PitchBack Check</button>
+                            </div>
+                        </div>
+                    )}
+
                 </div>
-            )}
 
-            {showNextSection && <StartProcessor />}
-
+                {showNextSection && <StartProcessor />}
+            </div>
         </div>
     );
 }
