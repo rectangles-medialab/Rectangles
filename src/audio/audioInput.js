6d750fa2f41f68b49a4a2144ff88bdec7ced22a1
NagalieArt <70587195+NagalieArt@users.noreply.github.com>
2023-03-28 10:16:40 +0200

    Added a handleVideoUploader for testing sound conversion in upload.

--- src/audio/audioInput.js
+++ src/audio/audioInput.js
@@ -3,10 +3,41 @@ import ml5 from 'ml5';
 
 function App() {
   const [videoFile, setVideoFile] = useState(null);
-
+  
   const handleVideoUpload = (event) => {
     // TODO: handle the video upload and conversion
-  }
+    const file = event.target.files[0];
+  
+    // Check if the file is an MP4 video
+    if (file.type !== 'video/mp4') {
+      alert('Please upload an MP4 video file.');
+      return;
+    }
+    // Create a new FileReader to read the contents of the video file
+    const reader = new FileReader();
+  
+    // Set up the callback function for when the reader is done reading the file
+    reader.onloadend = () => {
+      // Create a new AudioContext to convert the video to an audio file
+      const audioContext = new AudioContext();
+      const audioSrc = audioContext.createMediaElementSource(new Audio(reader.result));
+      const recorder = new Recorder(audioSrc);
+  
+      // Start recording the audio
+      recorder.record();
+  
+      // Set a timeout to stop recording after the video length
+      setTimeout(() => {
+        recorder.stop();
+        recorder.exportWAV((blob) => {
+          // TODO: Pass the audio file to the function that listens for "uh" sounds
+        });
+      }, new Audio(reader.result).duration * 1000);
+    };
+  
+    // Read the contents of the video file
+    reader.readAsDataURL(file);
+  };
 
   const listenForUhSounds = (audioFile) => {
     const soundClassifier = ml5.soundClassifier('your-trained-model');
---
+++ src/audio/ml5.d.ts
@@ -0,0 +1,5 @@
+declare module 'ml5' {
+    const ml5: any;
+    export default ml5;
+  }
+  
