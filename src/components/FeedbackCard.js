da8d5d6843d93dff0f448e0206020c7ad178deaa
Rav1sh <ravishsomai808@gmail.com>
2023-03-28 13:57:05 +0200

    Added colors to the feedback cards

--- src/components/FeedbackCard.js
+++ src/components/FeedbackCard.js
@@ -3,21 +3,23 @@ import React from "react";
 function FeedbackCard() {
     return (
             <div class="card-container">
-                <div class="card">
-                    <h2>Feedback 1</h2>
-                    <p>Hier komt feedback 1 over je pitch</p>
+                <div class="card positive">
+                    <h2>Postuur</h2>
+                    <p>Je postuur is uitstekend in jouw pitch, ga zo door hiermee.</p>
                 </div>
 
-                <div class="card">
-                    <h2>Feedback 2</h2>
-                <p>Hier komt feedback 2 over je pitch</p>
+                <div class="card positive">
+                    <h2>Pitch Lengte</h2>
+                <p>De lengte van je pitch is precies goed, houd deze tijd aan.</p>
                 </div>
 
-                <div class="card">
-                    <h2>Feedback 3</h2>
-                <p>Hier komt feedback 3 over je pitch</p>
+            <div class="card negative">
+                    <h2>Uhmmm</h2>
+                <p>Probeer in je volgende pitch minder "Uhm" te zeggen.</p>
                 </div>
             </div>
+
+            
     );
 }
 
