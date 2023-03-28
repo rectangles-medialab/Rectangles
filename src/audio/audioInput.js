df6001cdc2d619fbf80763d9ce3690965c68b840
NagalieArt <70587195+NagalieArt@users.noreply.github.com>
2023-03-28 10:03:58 +0200

    Worked on Ml5 code training sound

--- src/audio/audioInput.js
+++ src/audio/audioInput.js
@@ -1,62 +1,34 @@
-import React, { useEffect, useState } from "react";
-import * as tf from "@tensorflow/tfjs";
-import * as speechCommands from "@tensorflow-models/speech-commands";
-import fs from "fs";
+import React, { useState } from 'react';
+import ml5 from 'ml5';
 
-function TeachableMachine() {
-  const [recognizer, setRecognizer] = useState(null);
-  const [classLabels, setClassLabels] = useState([]);
-  const [labelContainer, setLabelContainer] = useState(null);
+function App() {
+  const [videoFile, setVideoFile] = useState(null);
 
-  useEffect(() => {
-    async function loadModel() {
-      const modelURL = "https://teachablemachine.withgoogle.com/models/ptOfwlb7h/model.json";
-      const metadataURL = "https://teachablemachine.withgoogle.com/models/ptOfwlb7h/metadata.json";
-      const model = await tf.loadLayersModel(tf.io.browserFiles([modelURL, metadataURL]));
-      const commands = JSON.parse(await fs.promises.readFile(metadataURL));
+  const handleVideoUpload = (event) => {
+    // TODO: handle the video upload and conversion
+  }
 
-      const recognizer = speechCommands.create("BROWSER_FFT", null, model);
-      await recognizer.ensureModelLoaded();
-      setRecognizer(recognizer);
-      setClassLabels(commands.wordLabels);
+  const listenForUhSounds = (audioFile) => {
+    const soundClassifier = ml5.soundClassifier('your-trained-model');
 
-      const container = document.getElementById("label-container");
-      setLabelContainer(container);
-      for (let i = 0; i < commands.wordLabels.length; i++) {
-        container.appendChild(document.createElement("div"));
+    soundClassifier.classify(audioFile, (error, result) => {
+      if (error) {
+        console.error(error);
+        return;
       }
 
-      recognizer.listen((result) => {
-        const scores = result.scores;
-        for (let i = 0; i < classLabels.length; i++) {
-          const classPrediction = classLabels[i] + ": " + result.scores[i].toFixed(2);
-          labelContainer.childNodes[i].innerHTML = classPrediction;
-        }
-      }, {
-        includeSpectrogram: true,
-        probabilityThreshold: 0.75,
-        invokeCallbackOnNoiseAndUnknown: true,
-        overlapFactor: 0.50
-      });
-    }
+      const uhCount = result.filter((classification) => classification.label === 'uh').length;
 
-    loadModel();
-
-    return () => {
-      if (recognizer) {
-        recognizer.stopListening();
+      if (uhCount >= 5) {
+        // TODO: trigger the desired functionality
       }
-    };
-  }, [recognizer, classLabels, labelContainer]);
+    });
+  }
 
   return (
     <div>
-      <div>Teachable Machine Audio Model</div>
-      <button type="button" onClick={() => recognizer && recognizer.listen()}>Start</button>
-      <div id="label-container"></div>
+      <input type="file" accept="video/mp4" onChange={handleVideoUpload} />
     </div>
   );
 }
-
-export default TeachableMachine;
 
