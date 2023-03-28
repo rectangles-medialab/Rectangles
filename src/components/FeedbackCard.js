2ed75db2141d8810705028be9d0d4d6439b922b6
Rav1sh <ravishsomai808@gmail.com>
2023-03-28 10:11:11 +0200

    Organized css

--- src/components/FeedbackCard.js
+++ src/components/FeedbackCard.js
@@ -5,17 +5,17 @@ function FeedbackCard() {
             <div class="card-container">
                 <div class="card">
                     <h2>Feedback 1</h2>
-                    <p>This is the content of card 1.</p>
+                    <p>Hier komt feedback 1 over je pitch</p>
                 </div>
 
                 <div class="card">
                     <h2>Feedback 2</h2>
-                    <p>This is the content of card 2.</p>
+                <p>Hier komt feedback 2 over je pitch</p>
                 </div>
 
                 <div class="card">
                     <h2>Feedback 3</h2>
-                    <p>This is the content of card 3.</p>
+                <p>Hier komt feedback 3 over je pitch</p>
                 </div>
             </div>
     );
