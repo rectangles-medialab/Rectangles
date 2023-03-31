2ef811e9eb29b0b7ce12673a346499ae193fa979
NagalieArt <70587195+NagalieArt@users.noreply.github.com>
2023-03-31 13:13:05 +0200

    Working on audio reg, not done

--- src/audio/audioInput.js
+++ src/audio/audioInput.js
@@ -1,38 +1,176 @@
-import { useState } from 'react';
-import VideoToAudio from 'video-to-audio'
-// import { createFFmpeg, fetchFile } from '@ffmpeg/ffmpeg';
-// const ffmpeg = createFFmpeg({ log: true });
+import React, { useState, useEffect } from 'react';
+import * as tf from '@tensorflow/tfjs';
+import * as speechCommands from '@tensorflow-models/speech-commands';
 
+function VideoInput() {
+  const [model, setModel] = useState(null);
+  const [metadata, setMetadata] = useState(null);
+  const [videoSrc, setVideoSrc] = useState(null);
+  const [prediction, setPrediction] = useState(null);
 
-function AudioInput() {
-//TODO: Upload file as mp4const [audioSrc, setAudioSrc] = useState('');
-const [audioSrc, setAudioSrc] = useState('');
+  const handleFileChange = async (event) => {
+    const file = event.target.files[0];
+    const videoUrl = URL.createObjectURL(file);
+    setVideoSrc(videoUrl);
+  };
 
-  async function convertToAudio(input) {
-    let sourceVideoFile = input.files[0];
-    let targetAudioFormat = 'mp3';
-    let convertedAudioDataObj = await VideoToAudio.convert(sourceVideoFile, targetAudioFormat);
-    let audioUrl = URL.createObjectURL(convertedAudioDataObj.blob);
-    setAudioSrc(audioUrl);
-  }
+  const handleVideoPlay = async () => {
+    const videoElement = document.getElementById('video');
+    const speechModel = await speechCommands.create('BROWSER_FFT');
+  
+    const audioData = await extractAudioData(videoElement);
+    const spectrogram = tf.tidy(() => {
+      const waveform = tf.tensor(audioData);
+      const pad = tf.zeros([speechModel.modelInputShape[0] - waveform.shape[0] % speechModel.modelInputShape[0]]);
+      const padded = tf.concat([waveform, pad], 0);
+      const framed = tf.signal.frame(padded, speechModel.modelInputShape[0], speechModel.modelInputShape[0] / 2).transpose();
+      const windowed = framed.mul(tf.signal.hammingWindow(speechModel.modelInputShape[0]));
+      const batched = windowed.reshape([windowed.shape[0], windowed.shape[1], 1]);
+      return speechModel.recognize(batched);
+    });
+  
+    setPrediction(spectrogram[0].toLowerCase());
+  };
+  
 
-  function handleFileInputChange(event) {
-    convertToAudio(event.target);
-  }
+  const extractAudioData = async (videoElement) => {
+    const mediaStream = await videoElement.captureStream();
+    const audioContext = new AudioContext();
+    const audioSourceNode = audioContext.createMediaStreamSource(mediaStream);
+    const analyserNode = audioContext.createAnalyser();
+    audioSourceNode.connect(analyserNode);
+    analyserNode.fftSize = 2048;
+    const bufferLength = analyserNode.frequencyBinCount;
+    const audioData = new Float32Array(bufferLength);
+    analyserNode.getFloatTimeDomainData(audioData);
+    return audioData;
+  };
+
+  useEffect(() => {
+    const loadModel = async () => {
+      const speechModel = await speechCommands.create('BROWSER_FFT', undefined, 'https://storage.googleapis.com/tfjs-models/tfjs/speech-commands/vocab.json', 'https://storage.googleapis.com/tfjs-models/tfjs/speech-commands/model.json');
+      await speechModel.ensureModelLoaded();
+      setModel(speechModel);
+      setMetadata(speechModel.modelMetadata);
+    };
+
+    loadModel();
+  }, []);
 
   return (
     <div>
-    <input type="file" accept="video/*" onChange={handleFileInputChange} />
-    {audioSrc && (
-      <audio controls src={audioSrc}>
-        Your browser does not support the audio element.
-      </audio>
-    )}
-  </div>
-);
+      <input type="file" accept="video/*" onChange={handleFileChange} />
+      {videoSrc && (
+        <div>
+          <video id="video" src={videoSrc} onPlay={handleVideoPlay} controls></video>
+        </div>
+      )}
+      {prediction && (
+        <div>
+          <p>Prediction:</p>
+          <p>{prediction}</p>
+        </div>
+      )}
+    </div>
+  );
 }
- 
+
+export default VideoInput;
+
+
+
+
+
+
+
+
+
+
+// import React, { useState, useEffect, useRef } from 'react';
+// import ml5 from 'ml5';
+// import model from './model.json';
+// import metadata from './metadata.json';
+
+// // https://teachablemachine.withgoogle.com/models/NGlGZN4Z6/
+
+// const VideoAnalyzer = () => {
+//   const [videoFile, setVideoFile] = useState(null);
+//   const [classificationResult, setClassificationResult] = useState(null);
+
+//   const videoRef = useRef(null);
+//   const classifierRef = useRef(null);
+
+//   useEffect(() => {
+//     if (videoFile) {
+//       const video = videoRef.current;
+
+//       const soundClassifierOptions = {
+//         includeSpectrogram: true,
+//         probabilityThreshold: 0.7,
+//         modelUrl: model,
+//         metadataUrl: metadata,
+//         audio: video
+//       };
+      
+//       const classifier = ml5.soundClassifier('model1', soundClassifierOptions, () => {
+//         console.log('Model loaded');
+//       });
+
+//       classifierRef.current = classifier;
+
+//       // Start classifying the video's audio frames
+//       classifierRef.current.classify((error, result) => {
+//         if (error) {
+//           console.error(error);
+//           return;
+//         }
+
+//         setClassificationResult(result);
+//       });
+
+//      // Set the video source and start playing
+//       video.srcObject = URL.createObjectURL(videoFile);
+//       video.play();
+
+//       return () => {
+//         // Stop the classifier when the component unmounts
+//         classifierRef.current.stop();
+//       };
+//     }
+//   }, [videoFile]);
+
+//   const handleVideoUpload = (event) => {
+//     setVideoFile(event.target.files[0]);
+//   };
+
+//   const handleClassificationResult = (result) => {
+//     if (result && classificationResult && result.label !== classificationResult.label) {
+//       setClassificationResult(result);
+//       console.log(result)
+//     }
+//   };
   
+
+//   return (
+//     <div>
+//       <input type="file" accept="video/*" onChange={handleVideoUpload} />
+//       {classificationResult && (
+//         <p>The current sound classification is: {classificationResult.label}</p>
+//       )}
+//       <video ref={videoRef} controls onPlay={() => classifierRef.current.classify(handleClassificationResult)} />
+//     </div>
+//   );
+// };
+
+// export default VideoAnalyzer;
+
+
+
+
+
+
+
+
 
 //TODO: MP4 gets automatically converted to MP3
 //TODO: MP3 will be analysed with ML5 (js)
@@ -90,6 +228,5 @@ const [audioSrc, setAudioSrc] = useState('');
 //     </div>
 // );
 
-export default AudioInput;
 
 
